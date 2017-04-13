30e2d9496f35b6ecef00953b1011b9b1de0f1191
dagreenl <dgreenlees28@hotmail.com>
2017-04-12 19:00:10 -0500

    Update multiCastServer.c

--- multiCastServer.c
+++ multiCastServer.c
@@ -1,74 +1,70 @@
-#include <sys/socket.h>
 #include <stdio.h>
 #include <string.h>
-#include <netinet/ip.h>
-#include <unistd.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/unistd.h>
 #include <stdlib.h>
+#include <errno.h>
+//Daniel Greenlees
+//Kevin Moore
 
-int main(int argc, char *argv[])
-{
-        int sock, nbytes, flags, addrlen, size, i, n, iPort;
-        struct sockaddr_in server, from;
-        char group[12], port[6], buffer[500];
-        u_char ttl;
+#include <sys/fcntl.h>
+#include <pthread.h>
+#define TARGET_PORT 22210
+#define TARGET_IP  "239.10.5.10"
 
-        if(argc != 2)
-        {
-                printf("%d ", argc);
-                printf("Expected one command, group number, to be passed to program\n");
+int main(int argc, char * argv[]){
+
+       time_t r_time;
+//        struct tm* l_time;
+        struct sockaddr_in server;
+        int i, sock, addrlen, size;
+        struct ip_mreq join_group;
+        char buffer[50];
+
+        memset(&server,0,sizeof(server));
+        server.sin_family = PF_INET;
+        server.sin_addr.s_addr= htonl(INADDR_ANY);
+        server.sin_port = htons(TARGET_PORT);
+
+//        time(&r_time);
+
+        // setup socket
+        if ((sock=socket(AF_INET, SOCK_DGRAM,0))<0){
+                printf("socket error = %d\n", sock);
                 return -1;
         }
-        strcpy(group, "239.10.5.");
-        strcat(group, argv[1]);
-        //Take the port in from the command line
-        memset(port, '\0', sizeof(port));
-        memset(port, '2', sizeof(char) * 3);
-        strcat(port, argv[1]);
-        //Creat socket
-        sock = socket(AF_INET, SOCK_DGRAM, 0);
-        if(sock < 0)
-        {
-                printf("Socket result: %d", sock);
+
+        // bind socket
+        i = bind(sock, (struct sockaddr*) &server, sizeof(server));
+        if( i< 0) {
+                printf("Main bind result: %d\n", i);
                 return -1;
         }
-        memset(buffer, '\0', sizeof(buffer));
-        ttl = 250;
-        iPort = atoi(port);
-        server.sin_port = htons(iPort);
-        //Bind the socket to the server
-        /*i = bind(sock, (struct sockaddr *) &server, sizeof(server));
-        if(i < 0)
-        {
-                printf("Bind result: %d\n", i);
+            
+        // send join request
+        join_group.imr_multiaddr.s_addr=inet_addr(TARGET_IP);
+    join_group.imr_interface.s_addr=htonl(INADDR_ANY);
+    if (setsockopt(sock,IPPROTO_IP,IP_ADD_MEMBERSHIP,&join_group,sizeof(join_group)) < 0) {
+                printf("Group join failed with error %d/n", errno);
                 return -1;
-        }*/
-        //Set the socket to be a multicast socket
-        setsockopt(sock, IPPROTO_IP, IP_MULTICAST_TTL, &ttl, sizeof(ttl));
-        //Set server structure information
-        server.sin_family = AF_INET;
-        server.sin_addr.s_addr = inet_addr(group);
-        //If the bind didn't fail then the server is ready to recieve a message
-        printf("Multicast UDP is ready\n\n");
-        //Set some variables for the recvfrom call
-        addrlen = sizeof(from);
-        nbytes = 500;
-        flags = 0;
-        n = 0;
-        while(1)
-        {
-                //Recieve a message, setting the bytes recieve to size, and 
-                //storing the message at buffer
-                sprintf(buffer, "This is message %d from Group %s beacon\n", n, argv[1]);
-                size = sendto(sock, buffer, sizeof(buffer), flags,
-                                (struct sockaddr *) &server, sizeof(server));
-                if(size < 0)
-                {
-                        perror("Sendto");
-                        exit(1);
+    }
+    printf("Group join succeded, waiting for broadcast/n");
+
+        // infinite loop to listen
+        while (1){
+                // If a message is received on the socket, print message and the current time
+                if ((size = recvfrom(sock,&buffer,sizeof(buffer),0,(struct sockaddr*)&server, &addrlen)) > 0){
+  //                      l_time = localtime(&r_time);
+                struct tm* l_time;
+                time(&r_time);
+                l_time = localtime(&r_time);
+
+                    printf("[%d-%d-%d %d:%d:%d] %s\n", l_time->tm_mday, l_time->tm_mon+1, l_time->tm_year+1900, l_time->tm_hour, l_time->tm_min, l_time->tm_sec, buffer);
                 }
-                printf("Message sent: %s\n", buffer);
-                n++;
-                sleep(1);
         }
         close(sock);
 }
+     
